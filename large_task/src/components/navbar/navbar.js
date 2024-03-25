809717590bdf1dae1680cf4b968836c62373e2dc
rowdysquirrel23 <24904759@edgehill.ac.uk>
2024-03-25 15:20:23 +0000

    content added to services page + some grid styling

--- large_task/src/components/navbar/navbar.js
+++ large_task/src/components/navbar/navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
         <div className='logo_name'>
         <img src={logo} alt='Logo  which is a blend of green and white that features simple depictions of high rise buildings'></img>
         {/* <Link to="/" className="dubai-landscapes"> Dubai Landscapes</Link> */}
-        <p>Dubai Landscapes</p>
+        <p className='logo_title'>Dubai Landscapes</p>
         </div>
         <ul>
             <ActiveLink to="/">Home</ActiveLink>
--- large_task/src/pages/services/servicesPage.js
+++ large_task/src/pages/services/servicesPage.js
@@ -1,5 +1,39 @@
 import './services.css';
+import image1 from '../../assets/4.jpg';
+import image2 from '../../assets/9.jpeg';
 
 export default function Services(){
-    return <h1>Services</h1>
+    return  <body className="services_body">
+            <h1 className="hero">FOR YOUR PERSONAL OASIS IN THE UAE</h1>
+            <h2 className="services_title">Landscaping Services</h2>
+            <section className="services_content">
+                <p className="initial_text">Dubai-Landscapes is one of the most experienced and reliable garden landscaping companies in the UAE. 
+                Experts in both soft and hard landscaping, we’re able to do it all, 
+                from providing perfect paving to enhancing gardens with flowers and plants that are able to withstand the UAE’s unique climate. 
+                Our team is full of local experts who have extensive knowledge of the type of plants that will work best for your outdoor space.
+                </p>
+                <section className="services_list">
+                    <p className="list_title">Our range of landscaping services includes:</p>
+                    <ul className="services_list">
+                        <li>• Gazebos</li>
+                        <li>• Pergolas</li>
+                        <li>• Decking</li>
+                        <li>• Paving</li>
+                        <li>• Water Features</li>
+                        <li>• Barbecue Areas</li>
+                        <li>• And much more!</li>
+                    </ul>
+                </section>
+                <p className="third_text">Our team can transform every garden or outdoor space, taking on board the unique requirements of our customers.
+                We never take a ‘one size fits all’ approach, instead tailoring our services specifically to the needs of our clients
+                </p>
+                <img src={image1} className="services_image1" alt="A garden with paving leading up to a house"></img>
+                <p className="fourth_text">A British-owned company, whose entire team is English managed, we always use products that have been manufactured using the highest quality materials. 
+                This includes award-winning artificial turf from Easigrass, a product that is 100% British designed as well as child and pet safe.
+                Operating across the entirety of Dubai and Abu Dhabi, whatever landscaping services you require, Dubai-Landscapes is here to help. 
+                Contact us by Whatsapp, on the phone or by filling out our online form, and a member of our team will be able to help.
+                </p>
+                <img src={image2} className="services_image2" alt="A garden with paving and a gazebo"></img>
+            </section>
+            </body>
 }
