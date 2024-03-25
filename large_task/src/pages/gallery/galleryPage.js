d62756453e5556c189f0beac03ee8d4c9d5ad887
rowdysquirrel23 <24904759@edgehill.ac.uk>
2024-03-25 19:51:41 +0000

    text content added to gallery page

--- large_task/src/pages/gallery/galleryPage.js
+++ large_task/src/pages/gallery/galleryPage.js
@@ -1,3 +1,13 @@
+import './gallery.css';
+
 export default function Gallery(){
-    return <h1>Gallery</h1>
+    return  <body className="gallery_body">
+                    <h1 className="hero_text">GRAB A GLIMPSE OF OUR WORK</h1>
+                <section className="gallery_text">
+                    <h2 className="gallery_projects">Our Projects</h2>
+                    <p className="gallery_description">Over the years, the team here at Dubai-Landscapes has worked on a whole host of projectsthroughout the UAE, helping to enhance the outdoor space of numerous clients in theregion. 
+                    While we could tell you about our best work, we thought it would be better to show youinstead. 
+                    On this page, you’ll find our projects gallery where you can browse through some ofour recent projects.</p>
+                </section>
+            </body>
 }
